f0a2643ac240b24ae30c8034dcd0f92e4fecd512
Nicolas De Jay <ndj@pinkfilter.org>
2015-05-25 11:20:22 -0400

    Add documentation to `DB::Task`

--- lib/ripe/db/task.rb
+++ lib/ripe/db/task.rb
@@ -1,19 +1,42 @@
 module Ripe
+
   module DB
+
+    ##
+    # This class represents a +Task+ object in ripe's internal database. Its
+    # fields are defined by +Ripe::DB::TaskMigration+.
+    #
+    # @see Ripe::WorkerController
+
     class Task < ActiveRecord::Base
       belongs_to :worker
+
+      ##
+      # Return path to task directory, which is the same as worker directory.
 
       def dir
         "#{self.worker.dir}"
       end
 
+      ##
+      # Return path to task-level combined +stdout+ and +stderr+ log.
+
       def log
         "#{self.dir}/#{self.id}.log"
       end
 
+      ##
+      # Return path to task-level job script, which only includes the task at
+      # hand.  This script is never actually executed by ripe.
+      #
+      # @see Ripe::DB::Worker#sh
+
       def sh
         "#{self.dir}/#{self.id}.sh"
       end
+
     end
+
   end
+
 end
--- lib/ripe/db/task_migration.rb
+++ lib/ripe/db/task_migration.rb
@@ -1,6 +1,16 @@
 module Ripe
+
   module DB
+  
+    ##
+    # This class creates and destroys the +Task+ model in the internal
+    # database.
+
     class TaskMigration < ActiveRecord::Migration
+
+      ##
+      # Create model in database.
+
       def self.up
         create_table :tasks do |t|
           t.belongs_to :worker
@@ -9,9 +19,15 @@ module Ripe
         end
       end
 
+      ##
+      # Destroy model in database.
+
       def self.down
         drop_table :tasks
       end
+
     end
+  
   end
+
 end
