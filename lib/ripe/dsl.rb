93f42468598df35b0e94f3e8bf435822e8339e38
Nicolas De Jay <ndj@pinkfilter.org>
2015-06-25 14:06:48 -0400

    Document `DSL` module

--- lib/ripe/dsl.rb
+++ lib/ripe/dsl.rb
@@ -1,2 +1,11 @@
+module Ripe
+
+  # This module contains `domain-specific language` syntactic sugar for
+  # defining workflows and tasks.
+
+  module DSL; end
+
+end
+
 require_relative 'dsl/task_dsl'
 require_relative 'dsl/workflow_dsl'
